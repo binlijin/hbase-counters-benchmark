ea26dd58a16dad3a7498b0f3b821cb52d1e75af6
Enis Soztutar <enis@apache.org>
2016-06-17 18:13:53 -0700

    Use 20 iterations

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@
 mvn clean install -DskipTests
 
 for i in 1 8 max;   do
-  java -jar target/benchmarks.jar -i 10 -wi 10 -t $i 2>&1 | tee results-threads-$i.log;
+  java -jar target/benchmarks.jar -i 20 -wi 20 -t $i 2>&1 | tee results-threads-$i.log;
 done
